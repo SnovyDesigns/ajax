69133cf03171e5a6bc6fbe36b5b00d38d9ffbfd3
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-05-10 17:52:50 +0200

    Finished 12.5

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+	var prefix = "https://cors-anywhere.herokuapp.com/",
+		tweetLink = "https://twitter.com/intent/tweet?text=",
+		quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+		$button = $('.trigger'),
+		$quote = $('.quote'),
+		$author= $('.author');
+
+	$(function() {
+		getQuote();
+		$('.trigger').click(function() {
+			getQuote();
+		});
+	});	
+
+	function getQuote () {
+		$.ajaxSetup({ cache: false });
+		$.getJSON(prefix + quoteUrl, createTweet);
+	}
+
+	function createTweet(input) {
+	    var data = input[0];
+
+	    var quoteText = $(data.content).text().trim();
+	    var quoteAuthor = data.title;
+
+	    if (!quoteAuthor.length) {
+	        quoteAuthor = "Unknown author";
+	    }
+
+	    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+	    if (tweetText.length > 140) {
+		    getQuote();
+		} else {
+		    var tweet = tweetLink + encodeURIComponent(tweetText);
+		    $('.quote').text(quoteText);
+		    $('.author').text("Author: " + quoteAuthor);
+		    $('.tweet').attr('href', tweet);
+		}
+	}
+	
+})(jQuery);	
